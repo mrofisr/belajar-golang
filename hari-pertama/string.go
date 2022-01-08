90cf88abb7179928ad7af2bf54686e22fb4949d0
mrofisr <rofim58@gmail.com>
2022-01-09 05:39:17 +0700

    Initial Init

---
+++ hari-pertama/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println("Hello World")
+	fmt.Println("Hello Coding World")
+
+	fmt.Println(len("Hello"))
+	fmt.Println("Hello World"[0])
+	fmt.Println("Hello Coding World"[1])
+}
---
+++ hari-pertama/variabel.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kata string
+
+	kata = "Hello World"
+	fmt.Println(kata)
+
+	kata = "Hello Coding World"
+	fmt.Println(kata)
+
+	// var kata = "Hello World"
+	// fmt.Println(kata)
+
+	// kata = "Hello Coding World"
+	// fmt.Println(kata)
+
+	// var age = 20
+	// fmt.Println(age)
+
+	// country := "Indonesia"
+	// fmt.Println(country)
+	var (
+		awal  = "Hello"
+		akhir = "World"
+	)
+	fmt.Println(awal, akhir)
+}
